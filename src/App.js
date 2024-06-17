89a49a5f46d6d8d8b8cfaa2dd9b5a0d514baab5f
Mihir Raj <202101131@daiict.ac.in>
2024-06-17 16:05:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FacultyPage from './pages/FacultyPage';
+
+function App() {
+  return (
+    <div className="App">
+      <FacultyPage/>
+    </div>
+  );
+}
+
+export default App;
