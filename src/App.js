7aa02d570f36926e1590fae7b23171482b595596
Priyank Hudka <141134658+Priyank-Hudka@users.noreply.github.com>
2024-06-22 14:19:53 +0530

    Faculty Page update
    Made Changes in the Faculty page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+=======
+import { Routes, Route } from 'react-router-dom';
+import './App.scss';
+>>>>>>> Stashed changes
 import FacultyPage from './pages/FacultyPage';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <FacultyPage/>
+=======
+
+      <Routes>
+        <Route path="/facultypage" element={<FacultyPage />} />
+      </Routes>
+
+>>>>>>> Stashed changes
     </div>
   );
 }
--- src/pages/FacultyPage.js
+++ src/pages/FacultyPage.js
@@ -2,119 +2,105 @@ import  logo from "../img/logo.webp"
 import React, { Component } from 'react'
 import Navbar from '../components/Navbar'
 import abhishek from '../img/Abhishek-Jindal.jpg';
-import './FacultyPage.css'
+import './StylePages.css'
+
 
 export default class FacultyPage extends Component {
   render() {
     return (
       <div>
         <Navbar/>
-        <h1>Faculty Information</h1><div>
+        <h1>Faculty Information</h1>
         <div className="faculty-cards-container">
-        <div className="card" style={{width: "20rem", height: "auto"}}>
-  <img src={abhishek} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title"><a href="/" className="card-link">Abhishek Jindal</a></h5>
-    <p className="card-text">PhD (Wireless Communications), IIT Delhi</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    
-    <li className="list-group-item"># 4101, FB-4, DA-IICT, Gandhinagar, Gujarat, India – 382007</li>
-    <li className="list-group-item">abhishek_jindal@daiict.ac.in</li>
-  </ul>
-  <div className="card-body">
-    <b>Specialization:-</b>
-    <p>Wireless Communication, Cyber-Physical Systems and Information Security</p>
-  </div>
-</div>
-<div className="card" style={{width: "18rem", height: "auto"}}>
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title"><a href="/" className="card-link">Aditya Tatu</a></h5>
-    <p className="card-text">PhD (Computer Science), University of Copenhagen, Denmark</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    
-    <li className="list-group-item">#1206, FB-1, DA-IICT, Gandhinagar, Gujarat, India – 382007</li>
-    <li className="list-group-item">aditya_tatu@daiict.ac.in</li>
-  </ul>
-  <div className="card-body">
-    <b>Specialization:-</b>
-    <p>Computer Vision, Image Processing, Pattern Recognition, Signal Processing</p>
-  </div>
-</div>
-<div className="card" style={{width: "18rem", height: "auto"}}>
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title"><a href="/" className="card-link">Amit Mankodi</a></h5>
-    <p className="card-text">PhD, DA-IICT Gandhinagar</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    
-    <li className="list-group-item"># 4205, FB-4, DA-IICT, Gandhinagar, Gujarat, India – 382007</li>
-    <li className="list-group-item">amit_mankodi@daiict.ac.in</li>
-  </ul>
-  <div className="card-body">
-    <b>Specialization:-</b>
-    <p>Embedded Systems, Computer Networks, High Performance Computing, Machine Learning</p>
-  </div>
-</div>
+          <div className="card" style={{width: "20rem", height: "auto"}}>
+            <img src={abhishek} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title"><a href="/" className="card-link">Abhishek Jindal</a></h5>
+              <p className="card-text">PhD (Wireless Communications), IIT Delhi</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <b>Specialization:-</b>
+                <p>Wireless Communication, Cyber-Physical Systems and Information Security</p>
+              </li>
+            </ul>
+          </div>
+          <div className="card" style={{width: "18rem", height: "auto"}}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title"><a href="/" className="card-link">Aditya Tatu</a></h5>
+              <p className="card-text">PhD (Computer Science), University of Copenhagen, Denmark</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <b>Specialization:-</b>
+                <p>Computer Vision, Image Processing, Pattern Recognition, Signal Processing</p>
+              </li>
+            </ul>
+          </div>
+          <div className="card" style={{width: "18rem", height: "auto"}}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title"><a href="/" className="card-link">Amit Mankodi</a></h5>
+              <p className="card-text">PhD, DA-IICT Gandhinagar</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <b>Specialization:-</b>
+                <p>Embedded Systems, Computer Networks, High Performance Computing, Machine Learning</p>
+              </li>
+            </ul>
+          </div>
 
-</div>
+        </div>
         <div><p></p></div>
-<div className="faculty-cards-container">
-        <div className="card" style={{width: "20rem", height: "auto"}}>
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title"><a href="/" className="card-link">Anil Roy</a></h5>
-    <p className="card-text">PhD (Physics), IIT Delhi</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    
-    <li className="list-group-item"># 1104, FB-1, DA-IICT, Gandhinagar, Gujarat, India – 382007</li>
-    <li className="list-group-item">anil_roy@daiict.ac.in</li>
-  </ul>
-  <div className="card-body">
-    <b>Specialization:-</b>
-    <p>Applications of Image Processing, Fiber Optics and Optical Communication, High speed Semiconductor Devices, Nanoscience and Nanotechnology, Quantum Optics, Technologies for Humanitarian Challenges</p>
-  </div>
-</div>
-<div className="card" style={{width: "18rem", height: "auto"}}>
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title"><a href="/" className="card-link">Anish Mathuria</a></h5>
-    <p className="card-text">PhD (Computer Science), University of Wollongong, Australia</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    
-    <li className="list-group-item"># 1105, FB-1, DA-IICT, Gandhinagar, Gujarat, India – 382007</li>
-    <li className="list-group-item">anish_mathuria@daiict.ac.in</li>
-  </ul>
-  <div className="card-body">
-    <b>Specialization:-</b>
-    <p>Network Security, Privacy-Preserving Computation, System and Software Security</p>
-  </div>
-</div>
-<div className="card" style={{width: "18rem", height: "auto"}}>
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title"><a href="/" className="card-link">Arnab Kumar Ray</a></h5>
-    <p className="card-text">PhD (Physics), Jadavpur University, Kolkata</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    
-    <li className="list-group-item"># 2210, FB-2, DA-IICT, Gandhinagar, Gujarat, India – 382007</li>
-    <li className="list-group-item">arnab_kumar@daiict.ac.in</li>
-  </ul>
-  <div className="card-body">
-    <b>Specialization:-</b>
-    <p>Astrophysical Accretion, Fluid Dynamics, Nonlinear Systems</p>
-  </div>
-</div>
-
-</div>      
-<div><p></p></div>  
-        </div></div>
+        <div className="faculty-cards-container">
+          <div className="card" style={{width: "20rem", height: "auto"}}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title"><a href="/" className="card-link">Anil Roy</a></h5>
+              <p className="card-text">PhD (Physics), IIT Delhi</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <b>Specialization:-</b>
+                <p>Applications of Image Processing, Fiber Optics and Optical Communication, High speed Semiconductor Devices, Nanoscience and Nanotechnology, Quantum Optics, Technologies for Humanitarian Challenges</p>
+              </li>
+            </ul>
+          </div>
+          <div className="card" style={{width: "18rem", height: "auto"}}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title"><a href="/" className="card-link">Anish Mathuria</a></h5>
+              <p className="card-text">PhD (Computer Science), University of Wollongong, Australia</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              
+              <li className="list-group-item">
+                <b>Specialization:-</b>
+                <p>Network Security, Privacy-Preserving Computation, System and Software Security</p>
+              </li>
+            </ul>
+          </div>
+          <div className="card" style={{width: "18rem", height: "auto"}}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title"><a href="/" className="card-link">Arnab Kumar Ray</a></h5>
+              <p className="card-text">PhD (Physics), Jadavpur University, Kolkata</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <b>Specialization:-</b>
+                <p>Astrophysical Accretion, Fluid Dynamics, Nonlinear Systems</p>
+              </li>
+            </ul>
+          </div>
+        </div>      
+        <div><p></p></div> 
+        <div className="footer">
+          <p>&copy; 2024 Internship Connect. All rights reserved.</p>
+        </div>
+      </div>
     )
   }
 }
